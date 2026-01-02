23b85403995b518dd6a515cd9787f7e3d1b4cb96
motowrotka <motowrotka@gmail.com>
2026-01-02 12:19:46 +0100

    Update xlsxwriter_pb_wrapper.cpp

--- xlsxwriter_pb_wrapper.cpp
+++ xlsxwriter_pb_wrapper.cpp
@@ -393,24 +393,4 @@ int __stdcall pb_worksheet_autofit_column(
     return worksheet_set_column(w, col, col, width, safe_fmt(format));
 }
 
-
-// ------------------------------------------------------------
-//  Version info
-// ------------------------------------------------------------
-__declspec(dllexport) const char* __stdcall pb_get_version() {
-    return "libxlsxwriter PowerBuilder Wrapper v1.3 (UTF-8 + wrap text, 32-bit rows/cols)";
-}
-
-uint32_t pbcolor_to_excel(uint32_t pb)
-{
-    uint32_t r = (pb & 0x00FF0000) >> 16; // pierwszy bajt: F0
-    uint32_t g = (pb & 0x0000FF00);       // środkowy bajt: CA (już na swojej pozycji po <<8)
-    uint32_t b = (pb & 0x000000FF) << 16; // ostatni bajt: A6 przeniesiony na początek
-
-    return r | g | b;                     // A6 CA F0
-}
-
-
-__declspec(dllexport) uint32_t __stdcall pb_convert_color(uint32_t pb_color) { return pbcolor_to_excel(pb_color); }
-
 } // extern "C"
